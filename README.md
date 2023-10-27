3e8cd8e5b5ca317f58af5ef40bb4fc146ff99c4d
316austin316 <115892533+316austin316@users.noreply.github.com>
2023-10-27 06:06:15 -0500

    removed functions and added better header support
    ugh so i had to remove some functions and commit to one format

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # CTXR-Converter
-Convert CTXR PC files for MGS3HD to popular image formats (PNG, BMP, DDS) and vice versa.
+Convert CTXR PC files for MGS3HD to PNG and vice versa.
 MGS2HD PC coming soon...
 
 ## Features
 
-- Convert CTXR to PNG, BMP, or DDS formats.
-- Convert PNG, BMP, or DDS back to CTXR.
+- Convert CTXR to PNG format.
+- Convert PNG back to CTXR.
 - Batch conversion support for multiple files.
--- ctxr3.py
+++ ctxr3.py
@@ -15,10 +15,12 @@ def open_file():
 
     with open(file_path, 'rb') as file:
         ctxr_header = file.read(132)
-        pixel_data = file.read()
+        pixel_data_length = struct.unpack_from('>I', ctxr_header, 0x80)[0]
+        pixel_data = file.read(pixel_data_length)
     
         width = struct.unpack_from('>H', ctxr_header, 8)[0]
         height = struct.unpack_from('>H', ctxr_header, 10)[0]
+        mipmap_data = struct.unpack_from('>B', ctxr_header, 0x26)[0]
     
     image_bgra = Image.frombytes('RGBA', (width, height), pixel_data)
     r, g, b, a = image_bgra.split()
@@ -30,7 +32,6 @@ def open_file():
     else:
         image_rgba.save(output_file_path, chosen_format.get().upper(), compress_level=0)
 
-
     label.config(text=f"File saved as {output_file_path}")
 
 
@@ -41,10 +42,10 @@ def save_as_ctxr():
         label.config(text="Please open a CTXR file first.")
         return
 
-    file_path = filedialog.askopenfilename(title="Select an image file", filetypes=[("Image files", "*.bmp;*.png;*.dds")])
+    file_path = filedialog.askopenfilename(title="Select an image file", filetypes=[("Image files", ".png")])
     if not file_path:
         return
-
+    
     # Handle DDS separately using imageio
     if file_path.endswith('.dds'):
         image_array = imageio.imread(file_path)
@@ -58,15 +59,22 @@ def save_as_ctxr():
 
     r, g, b, a = image.split()
     image_bgra = Image.merge("RGBA", (b, g, r, a))
-
-    # Adjust ctxr_file_path construction to also replace .dds with .ctxr
+    
+    ctxr_header = bytearray(ctxr_header)
+    struct.pack_into('>H', ctxr_header, 8, image.width)
+    struct.pack_into('>H', ctxr_header, 10, image.height)
+    struct.pack_into('>I', ctxr_header, 0x80, len(image_bgra.tobytes()) + 28)  # Accounting for padding only
+    
     ctxr_file_path = file_path.rsplit('.', 1)[0] + '.ctxr'
     
     with open(ctxr_file_path, 'wb') as file:
         file.write(ctxr_header)
         file.write(image_bgra.tobytes())
+        file.write(b'\x00' * 28)  # Padding
 
     label.config(text=f"File saved as {ctxr_file_path}")
+
+
 
     
 def batch_convert_ctxr_to_png():
@@ -80,7 +88,8 @@ def batch_convert_ctxr_to_png():
                 file_path = os.path.join(root, file)
                 with open(file_path, 'rb') as f:
                     ctxr_header = f.read(132)
-                    pixel_data = f.read()
+                    pixel_data_length = struct.unpack_from('>I', ctxr_header, 0x80)[0]
+                    pixel_data = f.read(pixel_data_length)
                 
                     width = struct.unpack_from('>H', ctxr_header, 8)[0]
                     height = struct.unpack_from('>H', ctxr_header, 10)[0]
@@ -94,67 +103,7 @@ def batch_convert_ctxr_to_png():
                 image_rgba.save(png_file_path, 'PNG', compress_level=0)
 
     label.config(text=f"Conversion Completed for folder {folder_path}")
-    
-def batch_convert_ctxr_to_dds():
-    folder_path = filedialog.askdirectory(title="Select a folder with CTXR files")
-    if not folder_path:
-        return
 
-    for root, dirs, files in os.walk(folder_path):
-        for file in files:
-            if file.endswith('.ctxr'):
-                file_path = os.path.join(root, file)
-                with open(file_path, 'rb') as f:
-                    ctxr_header = f.read(132)
-                    pixel_data = f.read()
-                    
-                    width = struct.unpack_from('>H', ctxr_header, 8)[0]
-                    height = struct.unpack_from('>H', ctxr_header, 10)[0]
-                
-                image_bgra = Image.frombytes('RGBA', (width, height), pixel_data)
-                r, g, b, a = image_bgra.split()
-                image_rgba = Image.merge("RGBA", (b, g, r, a))
-                
-                dds_file_path = file_path.replace('.ctxr', '.dds')
-                imageio.imwrite(dds_file_path, image_rgba)
-
-    label.config(text=f"Conversion Completed for folder {folder_path}")
-    
-def batch_convert_dds_to_ctxr():
-    dds_folder_path = filedialog.askdirectory(title="Select a folder with DDS files")
-    ctxr_folder_path = filedialog.askdirectory(title="Select a folder with original CTXR files for headers")
-    
-    if not dds_folder_path or not ctxr_folder_path:
-        return
-
-    for root, dirs, files in os.walk(dds_folder_path):
-        for file in files:
-            if file.endswith('.dds'):
-                dds_file_path = os.path.join(root, file)
-                ctxr_file_path = os.path.join(ctxr_folder_path, file.replace('.dds', '.ctxr'))
-                
-                if not os.path.exists(ctxr_file_path):
-                    continue  # Skip if matching CTXR file doesn't exist
-
-                with open(ctxr_file_path, 'rb') as f:
-                    ctxr_header = f.read(132)
-
-                image = Image.open(dds_file_path)
-
-                if image.mode != "RGBA":
-                    image = image.convert("RGBA")
-
-                r, g, b, a = image.split()
-                image_bgra = Image.merge("RGBA", (b, g, r, a))
-                
-
-
-                new_ctxr_file_path = dds_file_path.replace('.dds', '_new.ctxr')
-                with open(new_ctxr_file_path, 'wb') as f:
-                    f.write(ctxr_header)
-                    f.write(image_bgra.tobytes())
-
-    label.config(text=f"Conversion Completed for folder {dds_folder_path}")
     
 
     
@@ -187,71 +136,20 @@ def batch_convert_png_to_ctxr():
                 
                 new_ctxr_file_path = png_file_path.replace('.png', '_new.ctxr')
                 with open(new_ctxr_file_path, 'wb') as f:
+                    # Update width, height and pixel data length in ctxr_header
+                    ctxr_header = bytearray(ctxr_header)
+                    struct.pack_into('>H', ctxr_header, 8, image.width)
+                    struct.pack_into('>H', ctxr_header, 10, image.height)
+                    struct.pack_into('>I', ctxr_header, 0x80, len(image_bgra.tobytes()) + 28)  # Including padding only
                     f.write(ctxr_header)
                     f.write(image_bgra.tobytes())
+                    f.write(b'\x00' * 28)  # Padding
 
     label.config(text=f"Conversion Completed for folder {png_folder_path}")
+
     
-def batch_convert_ctxr_to_bmp():
-    folder_path = filedialog.askdirectory(title="Select a folder with CTXR files")
-    if not folder_path:
-        return
-
-    for root, dirs, files in os.walk(folder_path):
-        for file in files:
-            if file.endswith('.ctxr'):
-                file_path = os.path.join(root, file)
-                with open(file_path, 'rb') as f:
-                    ctxr_header = f.read(132)
-                    pixel_data = f.read()
-                
-                    width = struct.unpack_from('>H', ctxr_header, 8)[0]
-                    height = struct.unpack_from('>H', ctxr_header, 10)[0]
-                    
-                image_bgra = Image.frombytes('RGBA', (width, height), pixel_data)
-
-                r, g, b, a = image_bgra.split()
-                image_rgba = Image.merge("RGBA", (b, g, r, a))
-
-                bmp_file_path = file_path.replace('.ctxr', '.bmp')
-                image_rgba.save(bmp_file_path, 'BMP')
-
-    label.config(text=f"Conversion Completed for folder {folder_path}")
 
 
-def batch_convert_bmp_to_ctxr():
-    bmp_folder_path = filedialog.askdirectory(title="Select a folder with BMP files")
-    ctxr_folder_path = filedialog.askdirectory(title="Select a folder with original CTXR files for headers")
-    
-    if not bmp_folder_path or not ctxr_folder_path:
-        return
-
-    for root, dirs, files in os.walk(bmp_folder_path):
-        for file in files:
-            if file.endswith('.bmp'):
-                bmp_file_path = os.path.join(root, file)
-                ctxr_file_path = os.path.join(ctxr_folder_path, file.replace('.bmp', '.ctxr'))
-                
-                if not os.path.exists(ctxr_file_path):
-                    continue  # Skip if matching CTXR file doesn't exist
-
-                with open(ctxr_file_path, 'rb') as f:
-                    ctxr_header = f.read(132)
-
-                image = Image.open(bmp_file_path)
-
-                if image.mode != "RGBA":
-                    image = image.convert("RGBA")
-
-                r, g, b, a = image.split()
-                image_bgra = Image.merge("RGBA", (b, g, r, a))
-                
-                new_ctxr_file_path = bmp_file_path.replace('.bmp', '_new.ctxr')
-                with open(new_ctxr_file_path, 'wb') as f:
-                    f.write(ctxr_header)
-                    f.write(image_bgra.tobytes())
-
-    label.config(text=f"Conversion Completed for folder {bmp_folder_path}")
 
 
 def batch_convert():
@@ -261,11 +159,7 @@ def batch_convert():
     # Define function mapping
     func_map = {
         'ctxr to png': batch_convert_ctxr_to_png,
-        'ctxr to dds': batch_convert_ctxr_to_dds,
-        'ctxr to bmp': batch_convert_ctxr_to_bmp,
-        'dds to ctxr': batch_convert_dds_to_ctxr,
         'png to ctxr': batch_convert_png_to_ctxr,
-        'bmp to ctxr': batch_convert_bmp_to_ctxr,
     }
 
     # Call the relevant function
@@ -293,7 +187,7 @@ chosen_batch_format = StringVar()
 frame = Frame(app)
 frame.pack(pady=10, padx=10)
 
-title = Label(frame, text="CTXR <-> BMP/PNG/DDS Converter", font=("Arial", 16, "bold"))
+title = Label(frame, text="CTXR <-> PNG Converter", font=("Arial", 16, "bold"))
 title.grid(row=0, column=0, columnspan=2, pady=10)
 
 description = Label(frame, text="For MGS3HD PC\nCode by 316austin316", font=("Arial", 10))
@@ -305,14 +199,14 @@ open_button.grid(row=2, column=0, pady=10, sticky="ew")
 save_button = Button(frame, text="Save as CTXR", command=save_as_ctxr, bg='#FF9800', fg='white', font=("Arial", 10, "bold"))
 save_button.grid(row=2, column=1, pady=10, sticky="ew")
 
-# Dropdown for BMP/PNG/DDS choice for individual conversion
-format_options = ["bmp", "png", "dds"]
+# Dropdown for PNG choice for individual conversion
+format_options = ["png"]
 chosen_format.set(format_options[0])  # set default value
 format_dropdown = OptionMenu(frame, chosen_format, *format_options)
 format_dropdown.grid(row=3, column=0, pady=10)
 
 # Dropdown for batch conversion format choice
-batch_format_options = ["ctxr to png", "ctxr to dds", "ctxr to bmp", "dds to ctxr", "png to ctxr", "bmp to ctxr"]
+batch_format_options = ["ctxr to png", "png to ctxr"]
 chosen_batch_format.set(batch_format_options[0])
 batch_format_dropdown = OptionMenu(frame, chosen_batch_format, *batch_format_options)
 batch_format_dropdown.grid(row=4, column=0, pady=10)
